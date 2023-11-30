29dfef0debe9ef2321f160af6c3b29e2d9fdf737
theandrunique <theandrusyper@gmail.com>
2023-11-30 22:57:52 +0300

    update config file

--- app/core/config.py
+++ app/core/config.py
@@ -7,11 +7,18 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
-    DB_ECHO: bool = False
+    db_echo: str
 
     @property
     def DATABASE_URI(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    @property
+    def DB_ECHO(self):
+        if self.db_echo == 1:
+            return True
+        else:
+            return False
 
     model_config = SettingsConfigDict(env_file=".env")
 
